f2cdb738ee06136305d85477a49befda3bf64f8c
hr576 <hectorrodrz576@gmail.com>
2025-10-29 09:46:44 -0300

    Añadir avances

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-asciidoctor-pdf -r asciidoctor-diagram ./RODRIGUEZ-HECTOR-TP-TECNICA-Y-TRABAJO.adoc -a imagesoutdir=images
+#asciidoctor-pdf -r asciidoctor-diagram ./RODRIGUEZ-HECTOR-TP-TECNICA-Y-TRABAJO.adoc -a imagesoutdir=images -a pdf-theme=./theme.yaml
+asciidoctor-pdf --theme theme.yml -r asciidoctor-diagram ./RODRIGUEZ-HECTOR-TP-TECNICA-Y-TRABAJO.adoc -a imagesoutdir=images
